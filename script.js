ef04df04bd7a987918c6fcf9e597156eef77627e
Maldonado73521 <123662317+Maldonado73521@users.noreply.github.com>
2023-01-26 09:29:21 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,29 @@
+/* Declare variables below to save the different sections (divs) of your story*/
+let StoryPart2Plane = document.querySelector(".option-one-screen");
+let StoryPart2Train = document.querySelector(".option-two-screen");
+let StoryPart3Plane = document.querySelector(".option-one-end");
+let StoryPart3Train = document.querySelector(".option-two-end");
+let OpeningMessage = document.querySelector(".message");
+let Question = document.querySelector(".question");
+let PlaneButton = document.querySelector(".option-one");
+
+
+
+// When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+
+
+//
+PlaneButton.onclick=function(){
+StoryPart2Plane.style.display="block";
+};
+//
+
+
+
+PlaneButton.onclick=function(){
+StoryPart3Plane.style.display="block";
+};
+//
+
+
+
